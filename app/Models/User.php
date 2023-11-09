3a14f9e714dd39415a5ae68f520512c73a953a63
workualemu <alemu.worku@gmail.com>
2023-11-09 07:50:06 +0300

    Jetstream installation

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 // use Laravel\Fortify\TwoFactorAuthenticatable;
-// use Laravel\Jetstream\HasProfilePhoto;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Usamamuneerchaudhary\Commentify\Traits\HasUserAvatar;
@@ -16,7 +16,7 @@ class User extends Authenticatable
 {
     use HasApiTokens;
     use HasFactory;
-    // use HasProfilePhoto;
+    use HasProfilePhoto;
     use HasRoles;
     use Notifiable;
     use HasUserAvatar;
